b2e1fcac92632a5d75593386d999a4989c6db862
sachindj <sachinjadhavdn@gmail.com>
2023-07-12 14:54:41 +0530

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -44,29 +44,29 @@ function Navbar() {
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="nav-link scrollto active" href="/">Home</a></li>
+          <li><a class="nav-link scrollto" href="/">About</a></li>
+          <li><a class="nav-link scrollto" href="/">Services</a></li>
+          <li><a class="nav-link   scrollto" href="/">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="/">Team</a></li>
+          <li class="dropdown"><a href="/"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+              <li class="dropdown"><a href="/"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
+                  <li><a href="/">Deep Drop Down 1</a></li>
+                  <li><a href="/">Deep Drop Down 2</a></li>
+                  <li><a href="/">Deep Drop Down 3</a></li>
+                  <li><a href="/">Deep Drop Down 4</a></li>
+                  <li><a href="/">Deep Drop Down 5</a></li>
                 </ul>
               </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="/">Drop Down 2</a></li>
+              <li><a href="/">Drop Down 3</a></li>
+              <li><a href="/">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="/">Contact</a></li>
           {user ? ( <li><a class="getstarted scrollto" style={{color : "red"}} onClick={handleLogout}>LogOut</a></li>):( <ul><li><a class="getstarted scrollto"  onClick={handleLogin}>Login</a></li>
           
           <li><a class="getstarted scrollto"  onClick={handleRegister}>Register</a></li></ul>
